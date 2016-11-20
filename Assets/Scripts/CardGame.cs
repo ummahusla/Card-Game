80dda77d1fd6e377bc40b953874679d72dc8ce87
Edvins Antonovs <ummahusla@gmail.com>
2016-11-20 11:29:52 +0000

    Added all private and public variables

---
+++ Assets/Scripts/CardGame.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CardGame : MonoBehaviour
+{
+
+    public int HandSize = 6;
+    public GameObject CardBack;
+
+    private GameObject[] Hand;
+    private GameObject[] EnemyHand;
+
+    public GameObject[] FairyDeck = new GameObject[6];
+    public GameObject[] WithcDeck = new GameObject[6];
+
+    private int[] MyCards = new int[6];
+    private int[] EnemyCards = new int[6];
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
