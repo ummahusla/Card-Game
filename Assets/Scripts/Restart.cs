1e0e3f7729638d80e2b6c36a087ebbaaf2494068
Edvins Antonovs <ummahusla@gmail.com>
2016-11-21 20:47:46 +0000

    Added basic reload level function

--- Assets/Scripts/Restart.cs
+++ Assets/Scripts/Restart.cs
@@ -3,13 +3,8 @@ using System.Collections;
 
 public class Restart : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    public void ReloadLevel() {
+        Application.LoadLevel("Game");
+    }
+    
 }
